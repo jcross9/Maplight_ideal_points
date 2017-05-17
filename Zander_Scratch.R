c08a90e66ced0d6a0727d5b756445b662ab0f45f
alexanderfurnas <zfurnas@gmail.com>
2017-05-17 16:22:14 -0400

    bill_ids

--- Zander_Scratch.R
+++ Zander_Scratch.R
@@ -5,6 +5,7 @@ pos114 <- read_csv("/Users/alexanderfurnas/Projects/Maplight_ideal_points/114pos
 
 pos114 <- pos114 %>% mutate(position_date = lubridate::ymd(stringr::str_split(citation, "[()]")[[1]][2]))
 
+pos114 <- pos114 %>% mutate(bill_id = paste(session, prefix, number, sep = "_"))
 
 
 library(lubridate)
