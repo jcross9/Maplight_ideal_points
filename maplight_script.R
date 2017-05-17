52e4d42ef2a7fe2cccdaf0fef8e5f4a9cc6e8f67
jcross9 <jcrosson9@gmail.com>
2017-05-17 16:15:00 -0400

    basic formatting

---
+++ maplight_script.R
@@ -0,0 +1,20 @@
+####################################################################################################
+########################### MAPLIGHT IDEAL POINT SCORES PROJECT SCRIPT #############################
+####################################################################################################
+
+
+############# PROJECT PROGRESS ##################
+
+## Looked at US repo to see if there are action timestamp. Link: 
+## Looked at how difficult it would be to create new column of dates from Maplight data (should be straightforward).
+
+
+
+############# DATA MANIPULATION AND CREATION #####################
+
+
+
+############# REQUIRED PACKAGES #################
+
+require("emIRT")
+
