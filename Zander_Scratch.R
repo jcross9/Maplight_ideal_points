279fe36ee3d914b47982915d4831b53473205238
alexanderfurnas <zfurnas@gmail.com>
2017-05-19 11:11:20 -0400

    added some code to count ratings and orgs

--- Zander_Scratch.R
+++ Zander_Scratch.R
@@ -4,7 +4,7 @@ library(lubridate)
 
 pos114 <- read_csv("/Users/alexanderfurnas/Projects/Maplight_ideal_points/114positions.csv")
 
-pos114 <- pos114 %>% mutate(position_date = lubridate::ymd(stringr::str_split(citation, "[()]")[[1]][2]))
+pos114 <- pos114 %>% mutate(position_date = lubridate::ymd(unlist(stringr::str_split(citation, "[()]")[2])))
 
 pos114 <- pos114 %>% mutate(bill_id = paste(session, prefix, number, sep = "_"))
 
@@ -13,9 +13,26 @@ pos114 <- pos114 %>% mutate(bill_id_index = as.numeric(as.factor(as.character(bi
 pos114 <- pos114 %>% mutate(org_index = as.numeric(as.factor(as.character(orgname))))
 
 pos114$disposition.dichot <- NA
+pos114$disposition.dichot[pos114$disposition == "oppose"] <- - 1
+pos114$disposition.dichot[pos114$disposition == "support"] <- 1
+
+org_poscounts <- pos114 %>% group_by(orgname) %>% summarise(num = n()) %>% filter( num>4) %>% arrange(desc(num))
+
+bill_orgcounts <- pos114 %>% group_by(bill_id) %>% summarise(num = n()) %>% filter( num>4) %>% arrange(desc(num))
+
+length(unique(pos114$bill_id))
+summary(pos114)
+
+pos114 <- pos114 %>% mutate(bill_id_index = as.numeric(as.factor(as.character(bill_id))))
+
+pos114 <- pos114 %>% mutate(org_index = as.numeric(as.factor(as.character(orgname))))
+
+# dichotomize disposition as numeric for vote matrix
+pos114$disposition.dichot <- NA
 pos114$disposition.dichot[pos114$disposition == "oppose"] <- -1
 pos114$disposition.dichot[pos114$disposition == "support"] <- 1
 
+#fill vote matrix
 est_mat <- NA
 m1 <- matrix(0, nrow = length(unique(pos114$org_index)), ncol = length(unique(pos114$bill_id_index)))
 est_mat <- pos114 %>% dplyr::select(16,15,17)
@@ -23,5 +40,3 @@ est_mat <- est_mat%>% filter(complete.cases(est_mat))
 est_mat <- as.matrix(est_mat)
 m1[est_mat[,1:2] ]<- as.numeric(est_mat[,3])
 print(dim(m1))
-
-
--- maplight_script.R
+++ maplight_script.R
@@ -27,6 +27,8 @@ library(lubridate)
 library(emIRT)
 library(pscl)
 
+
+
 pos114 <- read_csv("/Users/alexanderfurnas/Projects/Maplight_ideal_points/114positions.csv")
 pos114 <- read_csv("C:/Users/jcros/Documents/Maplight_ideal_points/114positions.csv")
 
@@ -46,10 +48,15 @@ pos114$disposition.dichot <- NA
 pos114$disposition.dichot[pos114$disposition == "oppose"] <- -1
 pos114$disposition.dichot[pos114$disposition == "support"] <- 1
 
+
+org_poscounts <- pos114 %>% group_by(orgname) %>% summarise(num = n()) %>% filter( num>4) %>% arrange(desc(num))
+
+bill_orgcounts <- pos114 %>% group_by(bill_id) %>% summarise(num = n()) %>% filter( num>4) %>% arrange(desc(num))
+
 #fill vote matrix
 est_mat <- NA
 m1 <- matrix(0, nrow = length(unique(pos114$org_index)), ncol = length(unique(pos114$bill_id_index)))
-est_mat <- pos114 %>% dplyr::select(16,15,17)
+est_mat <- pos114 %>% dplyr::select(org_index,bill_id_index, disposition.dichot)
 est_mat <- est_mat%>% filter(complete.cases(est_mat))
 est_mat <- as.matrix(est_mat)
 m1[est_mat[,1:2] ]<- as.numeric(est_mat[,3])
